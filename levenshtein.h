07dab8df88f74e20c1f2dfbb5a5bea5e33896b33
skylerlee <skyler.ac.lee@gmail.com>
2022-10-10 17:40:48 +0000

    Format code

--- levenshtein.h
+++ levenshtein.h
@@ -1,6 +1,7 @@
 #ifndef LEVENSHTEIN_H_
 #define LEVENSHTEIN_H_
 
-int levenshtein(const char* string1, const char* string2, int w, int s, int a, int d);
+int levenshtein(
+    const char* string1, const char* string2, int w, int s, int a, int d);
 
 #endif  // LEVENSHTEIN_H_
