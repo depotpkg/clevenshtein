3b02693f87ba185a7ac01553f75a32a2e395db32
skylerlee <skyler.ac.lee@gmail.com>
2022-10-10 18:12:59 +0000

    Add comment

--- levenshtein.h
+++ levenshtein.h
@@ -1,6 +1,17 @@
 #ifndef LEVENSHTEIN_H_
 #define LEVENSHTEIN_H_
 
+/**
+ * Calculate a Levenshtein distance between two strings.
+ *
+ * @param string1
+ * @param string2
+ * @param w: weight of "sWap" operation
+ * @param s: weight of "Substitution" operation
+ * @param a: weight of "Add" operation
+ * @param d: weight of "Deletion" operation
+ * @returns the weighted distance
+ */
 int levenshtein(
     const char* string1, const char* string2, int w, int s, int a, int d);
 
